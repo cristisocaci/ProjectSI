941a01ed09f9a65fdb09dc5cc243fc98e3c04321
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-10-20 12:59:11 +0300

    obtained xflat and yflat

--- Part1/ciorna.m
+++ Part1/ciorna.m
@@ -1,2 +1,21 @@
-b = load('proj_fit_18.mat');
-surf(b.id.X{1}, b.id.X{2}, b.id.Y)
+load('proj_fit_18.mat');
+X1 = id.X{1};
+X2 = id.X{2};
+Y = id.Y;
+surf(id.X{1}, id.X{2}, id.Y)
+xlabel("x1"); ylabel("x2")
+x1i = [];
+
+for i=1:length(X1)
+    x1i =[x1i ones(1,length(X2)) * X1(i)];
+end
+x2i = [];
+for i = 1:length(X2)
+    x2i = [x2i X2];
+end
+
+Xflat = [ x1i', x2i'];
+
+Yflat = reshape(Y, length(Y)^2, 1);
+
+[Xflat, Yflat]
