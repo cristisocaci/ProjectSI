e1b9559e908111a8d660fceb794119f3086a9ab7
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-10-20 16:16:04 +0300

    finished mse
    mse vs degree done => for m = 10 we have smallest mse

--- Part1/part1.m
+++ Part1/part1.m
@@ -4,20 +4,33 @@ id.X2 = id.X{2};
 val.X1 = val.X{1};
 val.X2 = val.X{2};
 
+maxdegree = 20;
 
-m = 4;
+mse = zeros(1, maxdegree);
 
-Xflat = flatten(id.X1,id.X2);
-Yflat = reshape(id.Y, length(id.Y)^2, 1);
+for m = 1:maxdegree
+    % Identification
+    Xflat = flatten(id.X1,id.X2);
+    Yflat = reshape(id.Y, length(id.Y)^2, 1);
+    regr = regressor(Xflat,m);
+    theta = regr\Yflat;
+    
+    % Validation
+    Xflatval = flatten(val.X1, val.X2);
+    Yflatval = reshape(val.Y, length(val.Y)^2, 1);
+    regr = regressor(Xflatval, m);
+    yhatflat = regr*theta;
+    yhat = reshape(yhatflat, length(val.X2), length(val.X2));
+    if m==10
+        yhatstar = yhat;
+    end
+    mse(m) = 1/length(Yflatval) * sum( (yhatflat - Yflatval).^2 );
+end
+subplot(211), plot(1:maxdegree, mse),title("MSE vs degree")
+subplot(212), plot(6:maxdegree, mse(6:end)), title("Zoom in")
+% from plot => MSE min is for m = 10
 
-regr = regressor(Xflat,m);
-theta = regr\Yflat;
-
-Xflatval = flatten(val.X1, val.X2);
-regr = regressor(Xflatval, m);
-yhatflat = regr*theta;
-yhat = reshape(yhatflat, length(val.X2), length(val.X2));
-
-surf(val.X1,val.X2, yhat)
+figure
+surf(val.X1,val.X2, yhatstar)
 hold on
 surf(val.X1, val.X2, val.Y, 'FaceAlpha',0.5),  xlabel("X1"); ylabel("X2"); zlabel("Y")
