d0566398bf73ca41c3ccfdd0bb34004f6ee94094
cristian-nicolae <nicolae.cristianmihai@gmail.com>
2020-10-22 12:08:34 +0300

    Update regressor.m

--- Part1/regressor.m
+++ Part1/regressor.m
@@ -1,6 +1,6 @@
 function [regr] = regressor(Xflat,degree)
     % We observed that a certain degree of the regressor contains all the
-    % terms that lower degrees contain; Ex: degree 0 contains "1"; degree 1
+    % terms that the lower degrees contain; Ex: degree 0 contains "1"; degree 1
     % contains degree 0 and "x1 x2"; degree 2 contains degree 0, 1 and
     % "x1^2 x2^2 x1x2"; this pattern applys for higher degrees also. In
     % order to create a regressor of a certain degree we create all the
