f1f590ae0956e320c2473ecfd9e8d528a4e2e8dc
cristian-nicolae <nicolae.cristianmihai@gmail.com>
2020-10-25 15:49:20 +0200

    Update degMatrix.m

--- Part1/degMatrix.m
+++ Part1/degMatrix.m
@@ -2,7 +2,7 @@ function [out] = degMatrix(X1,X2,deg)
     % In order to generate the required terms for the specified degree
     % We create an auxiliary matrix. For example, for degree 3 we create
     % aux = [x1 x1 x1 x2 x2 x2] and first we multiply the 3 x1's, 
-    % then with 2 x1's and one, then one x1 and 2 x2's, tean 3 x2's
+    % then with 2 x1's and one, then one x1 and 2 x2's, then 3 x2's
     
     out = ones(length(X1), deg+1);
     aux = zeros(length(X1), (deg)*2);
