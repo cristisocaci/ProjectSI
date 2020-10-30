2deeeba9c376ae447001bcfbc08ef4416be0a394
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-10-30 09:49:33 +0200

    small improvements

--- Part1/part1.m
+++ Part1/part1.m
@@ -1,13 +1,9 @@
 load('proj_fit_18.mat');
-id.X1 = id.X{1};
-id.X2 = id.X{2};
-val.X1 = val.X{1};
-val.X2 = val.X{2};
 
 % Flatten the data
-Xflat = flatten(id.X1,id.X2); 
+Xflat = flatten(id.X{1},id.X{2}); 
 Yflat = reshape(id.Y, length(id.Y)^2, 1);
-Xflatval = flatten(val.X1, val.X2);
+Xflatval = flatten(val.X{1}, val.X{2});
 Yflatval = reshape(val.Y, length(val.Y)^2, 1);
 
 maxdegree = 20;
@@ -20,17 +16,17 @@ for m = 1:maxdegree
     % Validation
     regr = regressor(Xflatval(:,1),Xflatval(:,2), m);
     yhatflat = regr*theta;
-    yhat = reshape(yhatflat, length(val.X2), length(val.X2));
+    yhat = reshape(yhatflat, length(val.X{2}), length(val.X{2}));
     if m==10
         yhatstar = yhat;
     end
-    mse(m) = 1/length(Yflatval) * sum( (yhatflat - Yflatval).^2 );
+    mse(m) = immse(yhat, val.Y);
 end
 subplot(211), plot(1:maxdegree, mse),title("MSE vs degree")
 subplot(212), plot(6:maxdegree, mse(6:end)), title("Zoom in")
 % from plot => MSE min is for m = 10
 msestar = min(mse);
 figure
-surf(val.X1,val.X2, yhatstar)
+surf(val.X{1},val.X{2}, yhatstar)
 hold on
-surf(val.X1, val.X2, val.Y, 'FaceAlpha',0.5),  xlabel("X1"); ylabel("X2"); zlabel("Y")
+surf(val.X{1}, val.X{2}, val.Y, 'FaceAlpha',0.5),  xlabel("X1"); ylabel("X2"); zlabel("Y")
