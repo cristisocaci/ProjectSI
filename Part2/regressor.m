a80b9dc390be0726eec68b0c822e423f2c87de38
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-12-15 12:06:12 +0200

    removed tic toc

--- Part2/regressor.m
+++ Part2/regressor.m
@@ -1,10 +1,8 @@
 function [regr] = regressor(d, pow)
-tic
     cases = length(pow);
     regr = ones(length(d(:,1)),cases);
     
     for i = 1:cases % To investigate parfor
         regr(:, i) = prod(d.^double(pow(i,:)),2);
     end
-toc
 end
