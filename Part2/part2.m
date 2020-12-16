40e8ff41fa60744e3190a12f723f730458490fcd
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-12-16 15:35:42 +0200

    changes

--- Part2/part2.m
+++ Part2/part2.m
@@ -1,14 +1,19 @@
+tic
 clear
 close all
 load("iddata-18.mat")
-% plot(id); title("Identification data")
-% figure, plot(val); title("Validation data")
+
 nmax=4;
 mmax=7;
 mse_pred_id=NaN(nmax,mmax);
 mse_pred_val=NaN(nmax,mmax);
 mse_sim_id=NaN(nmax,mmax);
 mse_sim_val=NaN(nmax,mmax);
+
+start=50;
+stop = length(id.y);
+l = start:stop;
+
 for n=1:nmax
     na = n; nb =n; nk = 1;
     
@@ -28,67 +33,51 @@ for n=1:nmax
     
     % simulation val
     ysim_val = simulate(theta, val.u, [na, nb, nk], pow)';
-    
-    start=50;
-    
-    mse_pred_id(n,m)=immse(ypred_id(start:end),id.y(start:end));
-    mse_pred_val(n,m)=immse(ypred_val(start:end),val.y(start:end));
-    mse_sim_id(n,m)=immse(ysim_id(start:end),id.y(start:end));
-    mse_sim_val(n,m)=immse(ysim_val(start:end),val.y(start:end));
+        
+    mse_pred_id(n,m)=immse(ypred_id(l),id.y(l));
+    mse_pred_val(n,m)=immse(ypred_val(l),val.y(l));
+    mse_sim_id(n,m)=immse(ysim_id(l),id.y(l));
+    mse_sim_val(n,m)=immse(ysim_val(l),val.y(l));
   end
 end
-[n_pred_id,m_pred_id]=bestmse(mse_pred_id);
-[n_pred_val,m_pred_val]=bestmse(mse_pred_val);
-[n_sim_id,m_sim_id]=bestmse(mse_sim_id);
-[n_sim_val,m_sim_val]=bestmse(mse_sim_val);
 
-na=n_pred_id; nb=n_pred_id; nk=1;
-m=m_pred_id;
+mseId = sqrt(mse_pred_id.^2 + mse_sim_id.^2);
+[nId,mId]=bestmse(mseId);
+mseVal = sqrt(mse_pred_val.^2 + mse_sim_val.^2);
+[nVal,mVal]=bestmse(mseVal);
+
+
+% Best identification plots
+na=nId; nb=nId; nk=1;
+m=mId;
 
 pow=generatePowers(m, na+nb);
 theta = train(id.y,id.u, [na, nb, nk], pow);
-
 ypred_id = predict(theta, id.y, id.u, [na, nb, nk], pow);
-
-figure
-plot(ypred_id(start:end), 'r'), hold on
-plot(id.y(start:end), 'b'), legend("Prediction data", "Identification data")
-
-na=n_pred_val; nb=n_pred_val; nk=1;
-m=m_pred_val;
-
-pow=generatePowers(m, na+nb);
-theta = train(id.y,id.u, [na, nb, nk], pow);
-
-ypred_id = predict(theta, val.y, val.u, [na, nb, nk], pow);
-
-figure
-plot(ypred_val(start:end), 'r'), hold on
-plot(val.y(start:end), 'b'), legend("Prediction data", "Validation data")
-
-na=n_sim_id; nb=n_sim_id; nk=1;
-m=m_sim_id;
-
-pow=generatePowers(m, na+nb);
-theta = train(id.y,id.u, [na, nb, nk], pow);
-
 ysim_id = simulate(theta, id.u, [na, nb, nk], pow);
 
 figure
-plot(ysim_id(start:end), 'r'), hold on
-plot(id.y(start:end), 'b'), legend("Simulation data", "Identification data")
+plot(l,ypred_id(l), 'r'), hold on
+plot(l,id.y(l), 'b'), legend("Prediction data", "Identification data") 
+figure
+plot(l,ypred_id(l), 'r'), hold on
+plot(l,id.y(l), 'b'), legend("Simulation data", "Identification data")
 
-na=n_sim_val; nb=n_sim_val; nk=1;
-m=m_sim_val;
+% Best validation plots
+na=nVal; nb=nVal; nk=1;
+m=mVal;
 
 pow=generatePowers(m, na+nb);
 theta = train(id.y,id.u, [na, nb, nk], pow);
-
+ypred_val = predict(theta, val.y, val.u, [na, nb, nk], pow);
 ysim_val = simulate(theta, val.u, [na, nb, nk], pow);
 
 figure
-plot(ysim_val(start:end), 'r'), hold on
-plot(val.y(start:end), 'b'), legend("Simulation data", "Validation data")
+plot(l,ypred_val(l), 'r'), hold on
+plot(l,val.y(l), 'b'), legend("Prediction data", "Validation data")
+figure
+plot(l,ysim_val(l), 'r'), hold on
+plot(l,val.y(l), 'b'), legend("Simulation data", "Validation data")
 
-
+toc
 
